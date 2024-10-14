50545dcc7f208f427efbfc553fe932404fdd910c
Divanny <divannyjpm@gmail.com>
2024-10-14 15:50:02 -0400

    Changing hero section dots color

--- CrowdSolve/CrowdSolve.Client/src/components/landing-page/HeroSection.jsx
+++ CrowdSolve/CrowdSolve.Client/src/components/landing-page/HeroSection.jsx
@@ -88,7 +88,7 @@ const HeroSection = () => {
                                 xmlns="http://www.w3.org/2000/svg"
                                 version="1.1"
                                 viewBox="0 0 800 800"
-                                className="size-full text-muted-foreground opacity-20"
+                                className="size-full opacity-50"
                             >
                                 {Array.from(Array(720).keys()).map((dot, index, array) => {
                                     const angle = 0.2 * index
@@ -103,6 +103,7 @@ const HeroSection = () => {
                                             cx={400 + x}
                                             cy={400 + y}
                                             opacity={1 - Math.sin(angle)}
+                                            fill="#9F5638"
                                         />
                                     )
                                 })}
