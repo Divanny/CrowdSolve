09c1fda3abc975e0b1ebac29234c232727d562c9
Divanny <divannyjpm@gmail.com>
2024-10-31 15:52:45 -0400

    Creating basic routes for company section and default component

---
+++ CrowdSolve/CrowdSolve.Client/src/pages/company/ChallengeForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChallengeForm = () => {
+    return (
+        <div className="bg-background text-foreground font-['Poppins, sans-serif] p-6">
+            <div className="container mx-auto mt-12">
+                
+            </div>
+        </div>
+    );
+};
+
+export default ChallengeForm;
